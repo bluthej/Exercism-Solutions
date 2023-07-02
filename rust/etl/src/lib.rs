b9e9d82f84a07ca2fcbfa27c1ab858c02c570acd
bluthej <joffrey.bluthe@e.email>
2023-07-02 22:09:15 +0200

    Add solution to etl

---
+++ rust/etl/src/lib.rs
@@ -0,0 +1,7 @@
+use std::collections::BTreeMap;
+
+pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    h.iter()
+        .flat_map(|(&value, letters)| letters.iter().map(move |l| (l.to_ascii_lowercase(), value)))
+        .collect()
+}
